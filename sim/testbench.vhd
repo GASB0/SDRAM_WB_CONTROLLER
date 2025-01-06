0a59146caee421379a1425913a4bcbd9d061da65
Gabriel <gsantos@deqx.com>
2025-01-06 11:11:02 -0400

    State machines almost complete

--- sim/testbench.vhd
+++ sim/testbench.vhd
@@ -6,10 +6,10 @@ entity tb_SDRAM_CONTROLLER is
 end tb_SDRAM_CONTROLLER;
 
 architecture behavior of tb_SDRAM_CONTROLLER is
-
     -- Component declaration for the DUT (Device Under Test)
     component SDRAM_CONTROLLER
         port(
+            -- SDRAM Side interface
             o_ADDR : out std_logic_vector(12 downto 0);
             o_BS   : out std_logic_vector(1 downto 0);
             io_DQ  : inout std_logic_vector(15 downto 0);
@@ -17,28 +17,49 @@ architecture behavior of tb_SDRAM_CONTROLLER is
             o_CASn : out std_logic;
             o_WEn  : out std_logic;
             o_CSn  : out std_logic;
-            o_LDQM : out std_logic;
-            o_UDQM : out std_logic;
+            SDRAM_DQM: inout std_logic_vector(1 downto 0);
             i_CLK  : in std_logic;
             i_CKE  : out std_logic;
-            resetn : in std_logic
+            resetn : in std_logic;
+
+            -- CPU access (WISHBONE SLAVE interface)
+            o_WB_ACK  : out std_ulogic;
+            i_WB_CLK  : in  std_ulogic;
+            i_WB_ADDR : in  std_ulogic_vector( 31 downto 0 );
+            i_WB_DAT  : in  std_ulogic_vector( 31 downto 0 );
+            o_WB_DAT  : out std_ulogic_vector( 31 downto 0 ) := (others => '0');
+            i_WB_RST  : in  std_ulogic;
+            i_WB_SEL  : in  std_ulogic_vector( 3 downto 0 );
+            i_WB_STB  : in  std_ulogic;
+            i_WB_WE   : in  std_ulogic;
+            i_WB_CYC  : in  std_ulogic
         );
     end component;
 
     -- Signals to connect to the DUT
-    signal s_ADDR : std_logic_vector(12 downto 0);
-    signal s_BS   : std_logic_vector(1 downto 0);
-    signal s_DQ   : std_logic_vector(15 downto 0);
-    signal s_RASn : std_logic;
-    signal s_CASn : std_logic;
-    signal s_WEn  : std_logic;
-    signal s_CSn  : std_logic;
-    signal s_LDQM : std_logic;
-    signal s_UDQM : std_logic;
-    signal s_CLK  : std_logic := '0';
-    signal s_CKE  : std_logic;
-    signal s_resetn : std_logic := '1';
+    signal s_ADDR      : std_logic_vector(12 downto 0) := (others => '0');
+    signal s_BS        : std_logic_vector(1 downto 0) := (others => '0');
+    signal s_DQ        : std_logic_vector(15 downto 0) := (others => '0');
+    signal s_RASn      : std_logic := '0';
+    signal s_CASn      : std_logic := '0';
+    signal s_WEn       : std_logic := '0';
+    signal s_CSn       : std_logic := '0';
+    signal s_SDRAM_DQM : std_logic_vector(1 downto 0) := (others => '0');
+    signal s_CLK       : std_logic := '0';
+    signal s_CKE       : std_logic := '0';
+    signal s_resetn    : std_logic := '1';
 
+    signal s_WB_ACK  : std_ulogic := '0';
+    signal s_WB_CLK  : std_ulogic := '0';
+    signal s_WB_ADDR : std_ulogic_vector( 31 downto 0 ) := (others => '0');
+    signal s_WB_DAT_i: std_ulogic_vector( 31 downto 0 ) := (others => '0');
+    signal s_WB_DAT_o: std_ulogic_vector( 31 downto 0 ) := (others => '0');
+    signal s_WB_RST  : std_ulogic := '0';
+    signal s_WB_SEL  : std_ulogic_vector( 3 downto 0 ) := (others => '0');
+    signal s_WB_STB  : std_ulogic := '0';
+    signal s_WB_WE   : std_ulogic := '0';
+    signal s_WB_CYC  : std_ulogic := '0';
+              
     -- Clock period definition for 100 MHz
     constant CLK_PERIOD : time := 10 ns;
 
@@ -54,11 +75,21 @@ begin
             o_CASn => s_CASn,
             o_WEn  => s_WEn,
             o_CSn  => s_CSn,
-            o_LDQM => s_LDQM,
-            o_UDQM => s_UDQM,
+            SDRAM_DQM => s_SDRAM_DQM,
             i_CLK  => s_CLK,
             i_CKE  => s_CKE,
-            resetn => s_resetn
+            resetn => s_resetn,
+            -- WB interface signals
+            o_WB_ACK  => s_WB_ACK,
+            i_WB_CLK  => s_WB_CLK,
+            i_WB_ADDR => s_WB_ADDR,
+            i_WB_DAT  => s_WB_DAT_i,
+            o_WB_DAT  => s_WB_DAT_o,
+            i_WB_RST  => s_WB_RST,
+            i_WB_SEL  => s_WB_SEL,
+            i_WB_STB  => s_WB_STB,
+            i_WB_WE   => s_WB_WE,
+            i_WB_CYC  => s_WB_CYC
         );
 
 
@@ -73,5 +104,24 @@ begin
         end loop;
     end process clk_gen;
     
+    -- Wishbone interface test
+    wb_test: process(s_CLK)
+      variable clk_cnt : unsigned(31 downto 0) := (others => '0');
+    begin
+      if rising_edge(s_CLK) then
+        if clk_cnt = to_unsigned(25000, clk_cnt'length) then
+          s_WB_ADDR <= std_ulogic_vector(to_unsigned(5, s_WB_ADDR'length));
+          s_WB_DAT_i <= std_ulogic_vector(to_unsigned(10, s_WB_ADDR'length));
+          s_WB_WE <= '0';
+          s_WB_SEL <= "0000";
+          s_WB_STB <= '1';
+          s_WB_CYC <= '1';
+        end if;
 
+        s_WB_STB <= '0' when s_WB_ACK ='1';
+        s_WB_CYC <= '0' when s_WB_ACK ='1';
+
+        clk_cnt := clk_cnt + 1;
+      end if;
+    end process wb_test;
 end behavior;
--- src/SDRAM_CONTROLLER.vhd
+++ src/SDRAM_CONTROLLER.vhd
@@ -8,6 +8,7 @@ use IEEE.MATH_REAL.ALL;
 
 entity SDRAM_CONTROLLER is
     port(
+      -- SDRAM Side interface
         o_ADDR : out std_logic_vector(12 downto 0);
         o_BS   : out std_logic_vector(1 downto 0);
         io_DQ  : inout std_logic_vector(15 downto 0);
@@ -15,18 +16,34 @@ entity SDRAM_CONTROLLER is
         o_CASn : out std_logic;
         o_WEn  : out std_logic;
         o_CSn  : out std_logic;
-        o_LDQM : out std_logic;
-        o_UDQM : out std_logic;
+        SDRAM_DQM: inout std_logic_vector(1 downto 0);
         i_CLK  : in std_logic;
         i_CKE  : out std_logic;
-        resetn : in std_logic
-        
+        resetn : in std_logic;
+
+        -- CPU access (WISHBONE SLAVE interface)
+        o_WB_ACK  : out std_ulogic;
+        i_WB_CLK  : in  std_ulogic;
+        i_WB_ADDR : in  std_ulogic_vector( 31 downto 0 );
+        i_WB_DAT  : in  std_ulogic_vector( 31 downto 0 );
+        o_WB_DAT  : out std_ulogic_vector( 31 downto 0 ) := (others => '0');
+        i_WB_RST  : in  std_ulogic;
+        i_WB_SEL  : in  std_ulogic_vector( 3 downto 0 );
+        i_WB_STB  : in  std_ulogic;
+        i_WB_WE   : in  std_ulogic;
+        i_WB_CYC  : in  std_ulogic
+
     );
 end SDRAM_CONTROLLER;
 
 architecture behavior of SDRAM_CONTROLLER is 
-    constant RFRSH_CYCLES : unsigned(9 downto 0) := to_unsigned(500, 10);
+    constant REFRESH_CYCLES : unsigned(9 downto 0) := to_unsigned(500, 10);
     constant FREQ : integer := 100_000_000;
+
+    -- Counter threshold constants for each state
+    constant PRECHARGE_ALL_CYCLES : integer := 3;
+    constant AUTO_REFRESH_CYCLES  : integer := 5;
+    constant SET_MODE_REG_CYCLES  : integer := 3;
 
     -- Defining SDRAM commands
     -- CS# RAS# CAS# WE#
@@ -39,7 +56,10 @@ architecture behavior of SDRAM_CONTROLLER is
     constant CMD_PreCharge    : std_logic_vector(3 downto 0) := "0010";
 
     type SDRAM_STATE is (s_INIT_DELAY, s_SETUP, s_NORMAL);
-    type SETUP_STATE is (s_PRECHARGE_ALL, s_AUTO_REFRESH, s_SET_MODE_REG, s_INIT_CONFIG_DONE);
+    type SETUP_STATE is (s_PRECHARGE_ALL, s_AUTO_REFRESH1, s_AUTO_REFRESH2, s_SET_MODE_REG, s_INIT_CONFIG_DONE);
+    type RW_STATE is (WAITING_RW_OPERATION, REFRESHING, EXECUTING_RW, FINISHING_RW);
+
+    signal r_RW_STATE : RW_STATE := WAITING_RW_OPERATION;
 
     signal r_SDRAM_STATE : SDRAM_STATE := s_INIT_DELAY;
     signal r_SETUP_STATE : SETUP_STATE := s_PRECHARGE_ALL;
@@ -51,12 +71,22 @@ architecture behavior of SDRAM_CONTROLLER is
     signal need_refresh : std_logic := '0';
     signal refresh_cnt  : unsigned(9 downto 0) := to_unsigned(501, 10);
     signal busy : std_logic := '0';
-    signal rst_done, rst_done_q : std_logic := '0';
+    signal rst_done, rst_done_q, i_WB_STB_q, begin_RW : std_logic := '0';
     signal rst_cnt  : unsigned(31 downto 0) := (others => '0');
     signal dq_oen : std_logic := '0';
-    signal SDRAM_DQM : std_logic_vector(1 downto 0) := "00";
+    signal dq_out : std_logic_vector(io_DQ'length-1 downto 0);
 
+    signal din_latch  : std_logic_vector(i_WB_DAT'length-1 downto 0);
+    signal addr_latch : std_logic_vector(i_WB_ADDR'length-1 downto 0);
+    signal we_latch   : std_logic := '0';
 begin
+
+    -- Inferred Latch for the ACK signal
+    o_WB_ACK <= '0' when i_WB_STB = '0' else
+                '1' when r_RW_STATE = FINISHING_RW;
+
+    io_DQ <= (others => 'Z') when dq_oen = '1' else
+             dq_out;
 
     -- Wiring the command register
     o_CSn  <= RAM_CMD(3);
@@ -70,7 +100,7 @@ begin
             -- RAM Row refresh indicator
             if (refresh_cnt = 0) then
                 need_refresh <= '0';
-            elsif (refresh_cnt >= RFRSH_CYCLES) then
+            elsif (refresh_cnt >= REFRESH_CYCLES) then
                 need_refresh <= '1';
             end if;
         end if;
@@ -84,9 +114,9 @@ begin
         if rising_edge(i_CLK) then
             -- Controller logic
             if not(resetn) then
-                busy <= '1';
-                dq_oen <= '1';
-                SDRAM_DQM <= "10";
+                busy          <= '1';
+                dq_oen        <= '1';
+                SDRAM_DQM     <= "10";
                 r_SDRAM_STATE <= s_INIT_DELAY;
             else 
                 -- defaults
@@ -100,49 +130,137 @@ begin
                         if cfg_now then
                             r_SDRAM_STATE <= s_SETUP;
                             r_SETUP_STATE <= s_PRECHARGE_ALL;
+                            RAM_CMD <= CMD_PreCharge;
                             v_CLK_CNT := (others => '0');
                         end if;
 
                     when s_SETUP =>
                     -- Issuing initial setup commands
-                        case r_SETUP_STATE is
-                            when s_PRECHARGE_ALL    =>
-                                RAM_CMD <= CMD_PreCharge when v_CLK_CNT = 1;
-                                r_SETUP_STATE <= s_AUTO_REFRESH when v_CLK_CNT = 3;
-                                v_CLK_CNT := (others => '0') when v_CLK_CNT = 3;
-                            when s_AUTO_REFRESH     =>
-                                RAM_CMD <= CMD_AutoRefresh when v_CLK_CNT = 1 or v_CLK_CNT = 3;
-                                r_SETUP_STATE <= s_SET_MODE_REG when v_CLK_CNT = 5;
-                                v_CLK_CNT := (others => '0') when v_CLK_CNT = 5;
-                            when s_SET_MODE_REG     =>
-                                RAM_CMD <= CMD_SetModeReg when v_CLK_CNT = 1;
-                                r_SETUP_STATE <= s_INIT_CONFIG_DONE when v_CLK_CNT = 3;
-                                v_CLK_CNT := (others => '0') when v_CLK_CNT = 3;
-                            when s_INIT_CONFIG_DONE =>
-                                r_SDRAM_STATE <= s_NORMAL;
-                                busy <= '0';
-                                r_SETUP_STATE <= s_PRECHARGE_ALL;
-                            when others =>
-                        end case;
+                      case r_SETUP_STATE is
+                        when s_PRECHARGE_ALL =>
+                          if v_CLK_CNT = 2 then
+                            r_SETUP_STATE <= s_AUTO_REFRESH1;
+                            RAM_CMD <= CMD_AutoRefresh;
+                            v_CLK_CNT := (others => '0');
+                          else
+                            v_CLK_CNT := v_CLK_CNT + 1;
+                          end if;
+                          
+                        when s_AUTO_REFRESH1 =>
+                          if v_CLK_CNT = 2 then
+                            r_SETUP_STATE <= s_AUTO_REFRESH2;
+                            RAM_CMD <= CMD_AutoRefresh;
+                            v_CLK_CNT := (others => '0');
+                          else
+                            v_CLK_CNT := v_CLK_CNT + 1;
+                          end if;
+
+                        when s_AUTO_REFRESH2=>
+                          if v_CLK_CNT = 2 then
+                            r_SETUP_STATE <= s_SET_MODE_REG;
+                            RAM_CMD <= CMD_SetModeReg;
+                            v_CLK_CNT := (others => '0');
+                          else
+                            v_CLK_CNT := v_CLK_CNT + 1;
+                          end if;
+
+                        when s_SET_MODE_REG =>
+                          if v_CLK_CNT = 2 then
+                            r_SETUP_STATE <= s_INIT_CONFIG_DONE; 
+                            v_CLK_CNT := (others => '0');
+                          else
+                            v_CLK_CNT := v_CLK_CNT + 1;
+                          end if;
+
+                        when s_INIT_CONFIG_DONE =>
+                          r_SDRAM_STATE <= s_NORMAL;
+                          busy <= '0';
+                          r_SETUP_STATE <= s_PRECHARGE_ALL;
+
+                        when others =>
+
+                      end case;
 
                     when s_NORMAL =>
+                        refresh_cnt <= refresh_cnt + 1 when refresh_cnt<= REFRESH_CYCLES;
 
-                    -- Refresh logic
-                        refresh_cnt <= refresh_cnt + 1;
+                        -- RW logic
+                        case r_RW_STATE is
+                          when WAITING_RW_OPERATION =>
+                            -- Refresh logic
+                            if need_refresh='1' and i_WB_CYC='0' then
+                              refresh_cnt <= to_unsigned(0, refresh_cnt'length);
+                              RAM_CMD <= CMD_AutoRefresh;
+                              r_RW_STATE <= REFRESHING;
+                            -- RW operations logic
+                            elsif begin_RW='1' then
+                              RAM_CMD <= CMD_Write when i_WB_WE='1' else
+                                         CMD_Read;         
+                              if i_WB_WE = '1' then
+                                dq_oen <= '0';
+                                dq_out <= din_latch(dq_out'length-1 downto 0);
+                              end if;
+                              o_ADDR <= "0010"&addr_latch(9 downto 1); -- Autoprecharged
+                              o_BS <= "00";
+                              r_RW_STATE <= EXECUTING_RW;
+                            end if;
 
-                        if need_refresh then -- Add more conditions to issue refresh
-                            refresh_cnt <= to_unsigned(0, refresh_cnt'length);
-                            RAM_CMD <= CMD_AutoRefresh;
-                        end if;
+                          when REFRESHING =>
+                            if v_CLK_CNT = 2 then
+                              r_RW_STATE <= WAITING_RW_OPERATION;
+                              v_CLK_CNT := (others => '0');
+                            else
+                              v_CLK_CNT := v_CLK_CNT + 1;
+                            end if;           
 
-                    -- Accessing banks logic
+                          when EXECUTING_RW =>
+                            if v_CLK_CNT = 2 then
+                              r_RW_STATE <= FINISHING_RW;
+                              v_CLK_CNT := (others => '0');
+                            else
+                              v_CLK_CNT := v_CLK_CNT + 1;
+                            end if;           
+
+                          when FINISHING_RW =>
+                            -- write the retrieved data in the output port
+                            r_RW_STATE <= WAITING_RW_OPERATION;
+                          when others =>
+                        end case;
+
                     when others =>
                 end case;
             end if;
-            v_CLK_CNT := v_CLK_CNT + 1;
         end if;
     end process STATE_MACHINE;
       
+    --
+    -- Capturing input signals
+    --
+    process(i_CLK)
+      variable v_begin_RW : std_logic := '0';
+    begin
+        if rising_edge(i_CLK) then
+          i_WB_STB_q <= i_WB_STB;
+          v_begin_RW := i_WB_STB and not(i_WB_STB_q);
+          begin_RW <= '0' when r_RW_STATE = WAITING_RW_OPERATION;
+
+          -- this is the section to get all the latches to their default values
+          if i_WB_CYC='0' then
+            addr_latch <= (others => '0');
+            din_latch  <= (others => '0');
+            we_latch   <= '0';
+          end if;
+
+          -- detect when chip is accessed and latch data from the ports
+          if v_begin_RW then
+            begin_RW   <= '1';
+            addr_latch <= i_WB_ADDR;
+            din_latch  <= i_WB_DAT;
+            we_latch   <= i_WB_WE;
+          end if;
+        end if;
+    end process;
+
     --
     -- Generate cfg_now pulse after initialization delay (normally 200us)
     --
